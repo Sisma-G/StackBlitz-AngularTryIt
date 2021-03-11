3d82963702e4e27c075f003b7c848c39a2389ff6
Sisma-G <giorgio.sismanidis@gmail.com>
2021-03-11 22:39:56 +0100

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { products } from "../products";
   styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent {
-  products = products;
+  products = products; //dafuq does that mean?
 
   share() {
     //Prompt a page alert with a message
